769646e5e8cba2e95fe42538e55c3354d560f600
caizhanjin <1362696697@qq.com>
2020-03-03 21:15:55 +0800

    20/03/03 update

--- research/research_strategies/num1_breaker/break_strategy_2.py
+++ research/research_strategies/num1_breaker/break_strategy_2.py
@@ -8,6 +8,9 @@ from vnpy.app.cta_strategy import (
     BarGenerator,
     ArrayManager,
     SampleBarData,
+    Interval,
+    Direction,
+    Offset,
 )
 from datetime import time
 
@@ -16,16 +19,20 @@ class BreakStrategy(CtaTemplate):
     """
     """
     author = "zhanjin"
-
-    break_rate = 0.4
-    stop_rate = 0.4
-    tend_length = 20
-
     fixed_size = 1
 
+    atr_length = 14
+    atr_break_rate = 1
+    fixed_stop_rate = 0.4
+
+    sar_price = 0
+    long_direction = ""
+    short_direction = ""
     long_stop = 0
     short_stop = 0
 
+    is_trade_today = False
+    is_break_today = False
     is_new_today = False
 
     parameters = ["fixed_size"]
@@ -39,8 +46,11 @@ class BreakStrategy(CtaTemplate):
         self.bg = BarGenerator(self.on_bar)
         self.am = ArrayManager()
 
-        self.am_day = ArrayManager(self.tend_length + 2)
+        self.bg_hour = BarGenerator(self.on_bar, 1, self.on_1hour_bar, Interval.HOUR)
+        self.am_hour = ArrayManager(20)
+
         self.bar_today = SampleBarData()
+        self.am_day = ArrayManager(20)
 
     def on_init(self):
         self.write_log("策略初始化")
@@ -58,18 +68,14 @@ class BreakStrategy(CtaTemplate):
     def on_bar(self, bar: BarData):
         self.cancel_all()
 
-        am = self.am
-        am.update_bar(bar)
+        self.am.update_bar(bar)
+        self.bg_hour.update_bar(bar)
 
         is_exit_time = time(hour=14, minute=55) <= bar.datetime.time() <= time(hour=15, minute=00)
         if not self.is_new_today and (bar.datetime.hour > 20 or bar.datetime.hour < 10):
             self.is_new_today = True
-
-            sar_value = self.am_day.sar()
-
-            print(bar.datetime)
-            print(bool(sar_value > self.am_day.low[-1]))
-
+            self.is_break_today = False
+            self.is_trade_today = False
             # 新的一天，更新昨天数据到 am_day
             if self.bar_today.open_price:
                 self.am_day.update_bar(self.bar_today)
@@ -86,10 +92,54 @@ class BreakStrategy(CtaTemplate):
             if is_exit_time:
                 self.is_new_today = False
 
-        if not am.inited:
+        if not self.am.inited:
+            return
+        if not self.am_hour.inited:
             return
         if not self.am_day.inited:
             return
+
+        atr_value = self.am_day.atr(self.atr_length)
+        atr_today = self.bar_today.high_price - self.bar_today.low_price
+        if atr_today > atr_value * self.atr_break_rate:
+            self.is_break_today = True
+
+        if not is_exit_time:
+            if self.pos == 0 and self.is_break_today and not self.is_trade_today:
+                if self.short_direction == "多" and self.long_direction == "多":
+                    self.buy(bar.close_price, self.fixed_size)
+                elif self.short_direction == "空" and self.long_direction == "空":
+                    self.short(bar.close_price, self.fixed_size)
+
+            elif self.pos > 0:
+                self.sell(self.long_stop, abs(self.pos), stop=True)
+
+            elif self.pos < 0:
+                self.cover(self.short_stop, abs(self.pos), stop=True)
+        else:
+            if self.pos > 0:
+                self.sell(bar.close_price * 0.99, abs(self.pos))
+            elif self.pos < 0:
+                self.cover(bar.close_price * 1.01, abs(self.pos))
+        self.put_event()
+
+    def on_1hour_bar(self, bar: BarData):
+        self.am_hour.update_bar(bar)
+        if not self.am_hour.inited:
+            return
+        if not self.am_day.inited:
+            return
+
+        short_sar_price = self.am_hour.sar()
+        long_sar_price = self.am_day.sar()
+
+        self.short_direction = "多" if bar.close_price > short_sar_price else "空"
+        self.long_direction = "多" if bar.close_price > long_sar_price else "空"
+
+        if self.short_direction == "多" and self.long_direction == "多":
+            self.long_stop = max(short_sar_price, long_sar_price)
+        elif self.short_direction == "空" and self.long_direction == "空":
+            self.short_stop = min(short_sar_price, long_sar_price)
 
         self.put_event()
 
@@ -97,6 +147,16 @@ class BreakStrategy(CtaTemplate):
         pass
 
     def on_trade(self, trade: TradeData):
+        # 开多
+        if trade.direction == Direction.LONG and trade.offset == Offset.OPEN:
+            self.is_trade_today = True
+            # self.long_stop = trade.price * (1 - self.fixed_stop_rate)
+
+        # 开空
+        elif trade.direction == Direction.SHORT and trade.offset == Offset.OPEN:
+            self.is_trade_today = True
+            # self.short_stop = trade.price * (1 + self.fixed_stop_rate)
+
         self.put_event()
 
     def on_stop_order(self, stop_order: StopOrder):

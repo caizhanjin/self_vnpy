e2c2ed77e0ae7d3284abcd97b4a45fe23f5911fc
caizhanjin <1362696697@qq.com>
2020-03-07 10:48:11 +0800

    20/03/07 update

--- research/research_strategies/num1_breaker/break_strategy_1.py
+++ research/research_strategies/num1_breaker/break_strategy_1.py
@@ -14,28 +14,30 @@ from datetime import time
 
 class BreakStrategy(CtaTemplate):
     """
-    r_break修改版：简化了逻辑，核心 突破+小止损/止盈
-    第一版：
+    日内atr突破策略
     """
-    author = "zhanjin"
+    author = "jin"
 
-    break_rate = 0.4
-    stop_rate = 0.4
+    fixed_size = 50
+
+    atr_length = 20
+    break_rate = 0.8
+    move_stop_rate = 0.4
+    fixed_stop_rate = 0.2
     tend_length = 20
 
-    fixed_size = 1
-
-    middle_line = 0  # 中轴
+    atr_value = 0
     sell_break = 0  # 突破卖出价
     buy_break = 0  # 突破买入价
 
     long_stop = 0
     short_stop = 0
+    is_trade_today = False
 
     is_new_today = False
 
-    parameters = ["break_rate", "stop_rate", "tend_length", "fixed_size"]
-    variables = ["buy_break", "sell_break", "middle_line", "long_stop", "short_stop"]
+    parameters = ["fixed_size", "break_rate", "move_stop_rate", "fixed_stop_rate"]
+    variables = []
 
     def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
         super(BreakStrategy, self).__init__(
@@ -70,14 +72,16 @@ class BreakStrategy(CtaTemplate):
         is_exit_time = time(hour=14, minute=55) <= bar.datetime.time() <= time(hour=15, minute=00)
         if not self.is_new_today and (bar.datetime.hour > 20 or bar.datetime.hour < 10):
             self.is_new_today = True
+
+            # 更新交易价格
+            self.is_trade_today = False
+            self.atr_value = self.am_day.atr(self.atr_length)
+            self.buy_break = bar.open_price + self.break_rate * self.atr_value
+            self.sell_break = bar.open_price - self.break_rate * self.atr_value
+
             # 新的一天，更新昨天数据到 am_day
             if self.bar_today.open_price:
                 self.am_day.update_bar(self.bar_today)
-
-                # 更新交易价格（每天只需计算一次）
-                self.middle_line = (self.bar_today.close_price + self.bar_today.low_price + self.bar_today.high_price) / 3
-                self.buy_break = self.middle_line + self.break_rate * (self.bar_today.high_price - self.bar_today.low_price)
-                self.sell_break = self.middle_line - self.break_rate * (self.bar_today.high_price - self.bar_today.low_price)
 
             self.bar_today.open_price = bar.open_price
             self.bar_today.high_price = bar.high_price
@@ -95,40 +99,37 @@ class BreakStrategy(CtaTemplate):
             return
         if not self.am_day.inited:
             return
-        if not self.middle_line:
-            return
 
         if not is_exit_time:
             if self.pos == 0:
-                self.long_stop = 0
-                self.short_stop = 0
-
                 # 过滤：下午14点后不交易
                 is_fit_trade = True
                 if 14 <= bar.datetime.hour <= 15:
                     is_fit_trade = False
                 # 过滤：趋势，20日均线
-                trend_am = self.am_day.sma(self.tend_length, True)
-                trend = 1 if trend_am[-1] > trend_am[-2] else -1
+                # trend_am = self.am_day.sma(self.tend_length, True)
+                # trend = 1 if trend_am[-1] > trend_am[-2] else -1
 
                 if is_fit_trade:
-                    long_entry = max(self.buy_break, self.bar_today.high_price)
-                    # self.buy(long_entry, self.fixed_size, stop=True)
-                    if trend == 1:
-                        self.buy(long_entry, self.fixed_size, stop=True)
+                    long_trade_price = max(self.bar_today.high_price, self.buy_break)
+                    self.long_stop = long_trade_price - self.fixed_stop_rate * self.atr_value
+                    self.buy(long_trade_price, self.fixed_size, stop=True)
+                    # if trend == 1:
+                    #     self.buy(long_entry, self.fixed_size, stop=True)
 
-                    short_entry = min(self.sell_break, self.bar_today.low_price)
-                    # self.short(short_entry, self.fixed_size, stop=True)
-                    if trend == -1:
-                        self.short(short_entry, self.fixed_size, stop=True)
+                    short_trade_price = min(self.bar_today.low_price, self.sell_break)
+                    self.short_stop = short_trade_price + self.fixed_stop_rate * self.atr_value
+                    self.short(short_trade_price, self.fixed_size, stop=True)
+                    # if trend == -1:
+                    #     self.short(short_entry, self.fixed_size, stop=True)
 
             elif self.pos > 0:
-                self.long_stop = self.bar_today.high_price * (1 - self.stop_rate / 100)
-                self.sell(self.long_stop, abs(self.pos), stop=True)
+                long_move_stop = self.bar_today.high_price - self.move_stop_rate * self.atr_value
+                self.sell(max(self.long_stop, long_move_stop), abs(self.pos), stop=True)
 
             elif self.pos < 0:
-                self.short_stop = self.bar_today.low_price * (1 + self.stop_rate / 100)
-                self.cover(self.short_stop, abs(self.pos), stop=True)
+                short_move_stop = self.bar_today.low_price + self.move_stop_rate * self.atr_value
+                self.cover(min(self.short_stop, short_move_stop), abs(self.pos), stop=True)
 
         else:
             if self.pos > 0:
@@ -142,6 +143,8 @@ class BreakStrategy(CtaTemplate):
         pass
 
     def on_trade(self, trade: TradeData):
+        self.is_trade_today = True
+
         self.put_event()
 
     def on_stop_order(self, stop_order: StopOrder):
